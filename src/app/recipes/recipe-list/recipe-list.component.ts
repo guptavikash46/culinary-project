c01bc2775e07393aaf1c2dc49cf4955ccef8507b
guptavikash46 <guptavikash46@gmail.com>
2019-12-26 02:07:05 +0530

    worked with recipe-list component to show dynamic data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe} from 'G:/Angular projects/culinery-project/src/app/recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [new Recipe('A test recipe', "This is simply a test-description", "https://get.pxhere.com/photo/dish-meal-food-recipe-breakfast-fast-food-squid-lunch-cuisine-delicious-rice-thailand-shrimp-asian-food-vegetarian-food-foodstuff-thailand-food-thai-food-side-dish-the-pork-fried-rice-made-southeast-asian-food-steamed-rice-stir-fried-seafood-a-fried-egg-plate-lunch-1377212.jpg")
+                    ];
   constructor() { }
 
   ngOnInit() {
