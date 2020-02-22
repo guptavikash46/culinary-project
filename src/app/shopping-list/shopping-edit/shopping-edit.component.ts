b8cf84ee5d7b842a201e77d8b8a41ff48596c4ca
guptavikash46 <guptavikash46@gmail.com>
2020-02-22 18:15:34 +0530

    event emitter & inter-component communication

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredients } from 'src/app/Models/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('itemName', {static: false}) itemName:ElementRef;
+  @ViewChild('itemAmount', {static: false}) itemAmount: ElementRef;
+  @Output() ingredientAdded  =new EventEmitter<Ingredients>();
+  //@Output() ingredientDeleted  =new EventEmitter<Ingredients>();
+  
+  constructor() { 
+    //console.log(this.val);
+  }
 
   ngOnInit() {
   }
+  onItemAdd(){
+    const ingr = new Ingredients(this.itemName.nativeElement.value, 
+     this.itemAmount.nativeElement.value);
+    this.ingredientAdded.emit(ingr);
+    //console.log(this.ingr.name+" "+this.ingr.amount);
 
+  }
+  // onItemDelete(){
+  //   const delItem = new Ingredients(this.itemName.nativeElement.value,
+  //     this.itemAmount.nativeElement.value);
+  //   this.ingredientDeleted.emit(delItem);
+  // }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,7 +10,23 @@ export class ShoppingListComponent implements OnInit {
   @Output() shoppingListNav = new EventEmitter<any>();
   ingredients: Ingredients[] = [ new Ingredients("Knobloch ", 120.65),
                                  new Ingredients("Kurkuma ", 112.65)];
-  constructor() { }
+                                 
+  ingrItemRecieved(data: Ingredients){
+    console.log(data);
+    this.ingredients.push(data);
+  }
+  ingrItemDelete(delIngr: Ingredients){
+    console.log(delIngr.name+" "+delIngr.amount);
+    let val = confirm("are you sure you want to delete?");
+    if(val){
+      // let index = this.ingredients.indexOf(delIngr);
+      // if(index === -1) alert("Ingredient not found.");
+      // else this.ingredients.splice(index, 1);
+      //delete this.ingredients[0];
+    }
+  }
+  constructor() { 
+  }
 
   ngOnInit() {
   }
